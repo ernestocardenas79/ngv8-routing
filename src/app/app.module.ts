fa67f68c62a17ce6edd054ee3c3e2b771152e083
Ernesto Cardenas Llanes <ernesto.llanes@neoris.cxnetworks.net>
2019-06-04 17:23:51 -0600

    Se agregaron los componentes home, user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
-import { ProductsModule } from './products/products.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 /* Features Modules*/
-import { PageNotFoundComponent } from './page-not-found.component';
-import { WelcomePageComponent } from './welcome-page.component';
-import { HomeComponent } from './home/home.component';
-import { UserModule } from './user/user.module';
-import { MessagesModule } from './messages/messages.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
-    WelcomePageComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ProductsModule,
-    UserModule,
-    MessagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
