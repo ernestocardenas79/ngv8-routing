06f299472775ef91fb947564454c3d05ef733d11
Ernesto Cardenas Llanes <ernesto.llanes@neoris.cxnetworks.net>
2019-06-04 16:55:04 -0600

    Se borran compoentes por que no corresponden al prefijo de la app

--- src/app/page-not-found.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  template: `
-  <h1>This is not the page you were looking for!</h1>
-  `
-})
-export class PageNotFoundComponent {}
-
